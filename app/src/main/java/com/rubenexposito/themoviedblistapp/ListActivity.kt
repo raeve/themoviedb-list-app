4b08a4da2e27372b43aef7187956c72c938bfc49
Ruben Exposito <hi@rubenexposito.com>
2018-12-08 17:50:15 +0100

    Started project

---
+++ app/src/main/java/com/rubenexposito/themoviedblistapp/ListActivity.kt
@@ -0,0 +1,12 @@
+package com.rubenexposito.themoviedblistapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list)
+    }
+}
